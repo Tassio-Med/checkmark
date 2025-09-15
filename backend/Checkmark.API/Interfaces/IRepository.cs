9f98a2ff552c7924a95eef5f7c4ea588ff45fa3a
Tássio Medeiros <tassiohfmed@gmail.com>
2025-09-15 20:58:08 -0300

    feat: Adiciona interface IRepository com métodos para operações CRUD

---
+++ backend/Checkmark.API/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Checkmark.API.Models;
+
+namespace Checkmark.API.Interfaces
+{
+  public interface IRepository<T> where T : BaseEntity
+  {
+    Task<T?> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> AddAsync(T entity);
+    Task<T?> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(int id);
+    Task<bool> ExixssAsync(int id);
+  }
+}
