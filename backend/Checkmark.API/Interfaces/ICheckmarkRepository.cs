3c1b5a9530426b9ed977b13679a40e694fadaf36
Tássio Medeiros <tassiohfmed@gmail.com>
2025-09-16 19:32:53 -0300

    feat: Adiciona interface ICheckmarkRepository com métodos para obter itens filtrados por prioridade

---
+++ backend/Checkmark.API/Interfaces/ICheckmarkRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Checkmark.API.Models;
+
+namespace Checkmark.API.Interfaces
+{
+  public interface ICheckmarkRepository : IRepository<CheckmarkItem>
+  {
+    Task<IEnumerable<CheckmarkItem>> GetCompletedItemsAsync();
+    Task<IEnumerable<CheckmarkItem>> GetPendingItemsAsync();
+    Task<IEnumerable<CheckmarkItem>> GetItemByPriorityAsync(ThreadPriorityLevel priority);
+
+  }
+}
