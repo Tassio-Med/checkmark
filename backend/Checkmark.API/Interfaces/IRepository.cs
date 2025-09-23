7474fb2c842a2a80b84927dc26e33741bba05ef1
Tássio Medeiros <tassiohfmed@gmail.com>
2025-09-23 19:30:36 -0300

    refactor: remove código comentado

--- backend/Checkmark.API/Interfaces/IRepository.cs
+++ backend/Checkmark.API/Interfaces/IRepository.cs
@@ -1,20 +1,3 @@
-/* using System.Collections.Generic;
-using System.Threading.Tasks;
-using Checkmark.API.Models;
-
-namespace Checkmark.API.Interfaces
-{
-  public interface IRepository<T> where T : BaseEntity
-  {
-    Task<T?> GetByIdAsync(int id);
-    Task<IEnumerable<T>> GetAllAsync();
-    Task AddAsync(T entity);
-    Task<T?> UpdateAsync(T entity);
-    Task<bool> DeleteAsync(int id);
-    Task<bool> ExistsAsync(int id);
-  }
-} */
-
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Checkmark.API.Models;
